7aa59d0ad6bcecc26eaac4dc97e974d472fe5956
MarioMihaly <mihaly.mario98@gmail.com>
2022-09-02 17:49:19 +0000

    Fixed exception when availability is missing

--- src/zoopla.py
+++ src/zoopla.py
@@ -10,14 +10,29 @@ class Zoopla:
         self.soup = BeautifulSoup(self.response.text, 'html.parser')
 
     def parse_listing(self, listing):
-        raw_title = listing.select('h2[data-testid="listing-title"]')[0].string
-        location = listing.select('p[data-testid="listing-description"]')[0].string
-        title = f'{raw_title} at {location}'
-        raw_url = listing.select('a[data-testid="listing-details-link"]')[0]['href']
-        link = Zoopla.PREPEND + re.sub(r'\?search_identifier=[a-f0-9]*', '', raw_url)
-        price = listing.select('p[class="css-1w7anck e4hp44v31"]')[0].string
-        available = listing.find_all('span', {'data-testid':'available-from-date'})[0].string[1:]
+        try:
+            raw_title = listing.select('h2[data-testid="listing-title"]')[0].string
+            location = listing.select('p[data-testid="listing-description"]')[0].string
+            title = f'{raw_title} at {location}'
+        except Exception:
+            title = 'Missing'
+        
+        try:
+            raw_url = listing.select('a[data-testid="listing-details-link"]')[0]['href']
+            link = Zoopla.PREPEND + re.sub(r'\?search_identifier=[a-f0-9]*', '', raw_url)
+        except Exception:
+            link = 'Missing'
 
+        try:
+            price = listing.select('div[data-testid="listing-price"]')[0].p.string
+        except Exception:
+            price = 'Missing'
+
+        try:
+            available = listing.find_all('span', {'data-testid':'available-from-date'})[0].string[1:]
+        except Exception:
+            available = 'Missing'
+            
         return (title, price, available, link)
 
     def get_listings(self):
