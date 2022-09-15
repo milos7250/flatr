684c146a1650fbcd96038ff09f861fda4ca4f9bf
MarioMihaly <mihaly.mario98@gmail.com>
2022-09-15 12:16:24 +0000

    Added template file for new sites

--- src/site_temp.py
+++ src/site_temp.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-class OnTheMarket:
-
-    PREPEND = 'onthemarket.co.uk'
+class ClassName:
+    PREPEND = 'prepend for website'
     HEADERS = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36',
         'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -11,7 +10,7 @@ class OnTheMarket:
     MISSING = 'Missing'
 
     def __init__(self, link):
-        self.response = requests.get(link, headers=OnTheMarket.HEADERS)
+        self.response = requests.get(link, headers=ClassName.HEADERS)
         self.soup = BeautifulSoup(self.response.text, 'html.parser')
 
     def parse_listing(self, listing):
@@ -25,32 +24,32 @@ class OnTheMarket:
 
     def get_title(self, listing):
         try:
-            raw_title = listing.select('span[class="title"]')[0].a.string
-            location = listing.select('span[class="address"]')[0].a.string
+            raw_title = listing.select('CSS selector')[0].a.string
+            location = listing.select('CSS selector')[0].a.string
             return f'{raw_title} at {location}'
 
         except:
-            return OnTheMarket.MISSING
+            return ClassName.MISSING
 
     def get_link(self, listing):
         try:
-            raw_link = listing.select('div[class="otm-PropertyCardMedia"]')[0].a['href']
-            return OnTheMarket.PREPEND + raw_link
+            raw_link = listing.select('CSS selector')[0].a['href']
+            return ClassName.PREPEND + raw_link
 
         except:
-            return OnTheMarket.MISSING
+            return ClassName.MISSING
 
     def get_price(self, listing):
         try:
-            return listing.select('div[class="otm-Price"]')[0].string
+            return listing.select('CSS selector')[0].string
         except:
-            return OnTheMarket.MISSING
+            return ClassName.MISSING
 
     def get_availability(self, listing):
-            return OnTheMarket.MISSING
+            return ClassName.MISSING
 
     def get_listings(self):
-        raw_listings = self.soup.find_all('li', {'class': 'otm-PropertyCard'})
+        raw_listings = self.soup.find_all('css tag', {'class': 'listing class'})
         listings = []
         for listing in raw_listings:
             listings.append(self.parse_listing(listing))
