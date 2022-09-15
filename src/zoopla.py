eaaf94b7ae54d4b1414e5796655838c03703e47f
MarioMihaly <mihaly.mario98@gmail.com>
2022-09-15 11:32:13 +0000

    Refactored Zoopla to standard format

--- src/zoopla.py
+++ src/zoopla.py
@@ -4,36 +4,51 @@ import re
 
 class Zoopla:
     PREPEND = 'zoopla.co.uk/'
+    MISSING = 'Missing'
 
     def __init__(self, link):
         self.response = requests.get(link)
         self.soup = BeautifulSoup(self.response.text, 'html.parser')
 
     def parse_listing(self, listing):
+        
+        title = self.get_title(listing)
+        link = self.get_link(listing)
+        price = self.get_price(listing)
+        available = self.get_availability(listing)
+            
+        return (title, price, available, link)
+
+    def get_title(self, listing):
         try:
             raw_title = listing.select('h2[data-testid="listing-title"]')[0].string
             location = listing.select('p[data-testid="listing-description"]')[0].string
-            title = f'{raw_title} at {location}'
+            return f'{raw_title} at {location}'
+
         except Exception:
-            title = 'Missing'
-        
+            return Zoopla.MISSING
+
+    def get_link(self, listing):
         try:
             raw_url = listing.select('a[data-testid="listing-details-link"]')[0]['href']
-            link = Zoopla.PREPEND + re.sub(r'\?search_identifier=[a-f0-9]*', '', raw_url)
+            return Zoopla.PREPEND + re.sub(r'\?search_identifier=[a-f0-9]*', '', raw_url)
+        
         except Exception:
-            link = 'Missing'
+            return Zoopla.MISSING
 
+    def get_price(self, listing):
         try:
-            price = listing.select('div[data-testid="listing-price"]')[0].p.string
+            return listing.select('div[data-testid="listing-price"]')[0].p.string
+        
         except Exception:
-            price = 'Missing'
+            return Zoopla.MISSING
 
+    def get_availability(self, listing):
         try:
-            available = listing.find_all('span', {'data-testid':'available-from-date'})[0].string[1:]
+            return listing.find_all('span', {'data-testid':'available-from-date'})[0].string[1:]
+        
         except Exception:
-            available = 'Missing'
-            
-        return (title, price, available, link)
+            return Zoopla.MISSING
 
     def get_listings(self):
         raw_listings = self.soup.find_all('div', {'data-testid': re.compile(r'search-result_listing_*')})
