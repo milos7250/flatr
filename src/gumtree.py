6416ff8479001fc1167dfeae5e76c2b7a14c8187
MarioMihaly <mihaly.mario98@gmail.com>
2022-09-15 11:37:48 +0000

    Refactored Gumtree to standard format

--- src/gumtree.py
+++ src/gumtree.py
@@ -3,21 +3,42 @@ from bs4 import BeautifulSoup
 
 class Gumtree:
     PREPEND = 'gumtree.co.uk'
+    MISSING = 'Missing'
 
     def __init__(self, link):
         self.response = requests.get(link)
         self.soup = BeautifulSoup(self.response.text, 'html.parser')
 
     def parse_listing(self, listing):
-        title = listing.select('.listing-title')[0].string.strip('\n')
-        link = Gumtree.PREPEND + listing.select('.listing-link')[0]['href']
-        price = listing.select('.listing-price')[0].strong.string
-        available = self.get_available(listing)
+        title = self.get_title(listing)
+        link = self.get_link(listing)
+        price = self.get_price(listing)
+        available = self.get_availability(listing)
 
         return (title, price, available, link)
 
+    def get_title(self, listing):
+        try:
+            return listing.select('.listing-title')[0].string.strip('\n')
+        
+        except Exception:
+            return Gumtree.MISSING
 
-    def get_available(self, listing):
+    def get_link(self, listing):
+        try:
+            return Gumtree.PREPEND + listing.select('.listing-link')[0]['href']
+
+        except Exception:
+            return Gumtree.MISSING
+
+    def get_price(self, listing):
+        try:
+            return listing.select('.listing-price')[0].strong.string
+
+        except Exception:
+            return Gumtree.MISSING
+
+    def get_availability(self, listing):
         attributes = dict()
 
         for li in listing.select('.listing-attributes')[0].findAll('li'):
@@ -28,7 +49,7 @@ class Gumtree:
         if 'Date available' in attributes:
             return attributes['Date available']
 
-        return 'N/A'
+        return Gumtree.MISSING
 
     def get_listings(self):
         raw_listings = self.soup.find_all('li', {'class':'natural'})
