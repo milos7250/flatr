a5d6eabc3a539d8ea585e2b5035b1a3d1a4a1d1f
MarioMihaly <mihaly.mario98@gmail.com>
2023-04-16 11:24:04 +0100

    Changed condition for failing sites

--- tests/test_sites.py
+++ tests/test_sites.py
@@ -6,7 +6,11 @@ PASS_SITES = [
     (sites.GrantProperty, 'https://www.grantproperty.com/tenants/properties/'),
     (sites.Gumtree, 'https://www.gumtree.com/search?search_category=property-to-rent'),
     (sites.OnTheMarket, 'https://www.onthemarket.com/to-rent/property/edinburgh/'),
-    (sites.Rightmove, 'https://www.rightmove.co.uk/property-to-rent/find.html?searchType=RENT&locationIdentifier=REGION%5E475&insId=1&radius=0.0&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&sortByPriceDescending=&_includeLetAgreed=on&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&letType=&letFurnishType=&houseFlatShare='),
+    (sites.Rightmove, 'https://www.rightmove.co.uk/property-to-rent/find.html?searchType=RENT\
+        &locationIdentifier=REGION%5E475&insId=1&radius=0.0&minPrice=&maxPrice=&minBedrooms=\
+        &maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&sortByPriceDescending=\
+        &_includeLetAgreed=on&primaryDisplayPropertyType=&secondaryDisplayPropertyType=\
+        &oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&letType=&letFurnishType=&houseFlatShare='),
     (sites.Spareroom, 'https://www.spareroom.co.uk/flatshare/index.cgi?&search_id=1212087156&offset=0&sort_by=days_since_placed'),
     (sites.ZoneLetting, 'https://zonegroup.co.uk/search-property/rent'),
     # (sites.Zoopla, 'https://www.zoopla.co.uk/to-rent/property/edinburgh-county/?price_frequency=per_month&q=Edinburgh&search_source=home')
@@ -37,9 +41,8 @@ def test_response_pass(site, link):
 
 
 @pytest.mark.parametrize('site, link', FAIL_SITES, ids=FAIL_IDS)
-@pytest.mark.xfail
 def test_response_fail(site, link):
-    assert site(link).response.status_code == 200
+    assert site(link).response.status_code != 200
 
 
 @pytest.mark.parametrize('site, link', PASS_SITES, ids=PASS_IDS)
