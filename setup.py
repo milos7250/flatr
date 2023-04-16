9045132181840d4a5c7e7c0f9e4d42c3816d2fa8
MarioMihaly <mihaly.mario98@gmail.com>
2023-04-16 12:09:55 +0100

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 if __name__ == '__main__':
     setup()
-
-# setup(name='flatr',
-#       version='1.0',
-#       description='Package to aid web scarping for flat hunting.',
-#       url='https://github.com/MarioMihaly/flatr',
-#       author='Mario Mihaly',
-#       author_email='mihaly.mario98@gmail.com',
-#       packages=find_packages()
-#     #   install_requires=['pandas', 'gspread', 'cachetools', 'requests', 'beautifulsoup4']
-#       )
